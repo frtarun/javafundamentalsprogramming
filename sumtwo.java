b251aada17ef0cd04a7fdfcff89faf800e6dda17
Tarun Singh Lodha <tarun.24jics195@jietjodhpur.ac.in>
2026-03-14 13:54:32 +0530

    Add sumtwo.java: calculates the sum of two numbers

---
+++ sumtwo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class sumtwo {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int n = sc.nextInt();
+        System.out.println("Enter the second number");
+        int m = sc.nextInt();
+        int sum = n + m;
+        System.out.println("The sum of " + n + " and " + m + " is " + sum);
+
+    }
+
+}
